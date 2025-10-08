f431de0c9c98c61b7a4f49dd49ee5e5b1ee1cbc9
Fokko Driesprong <fokko@apache.org>
2025-10-08 08:41:57 +0200

    Allow converting `bytes::Bytes` into a Binary Scalar (#1373)
    ## What changes are proposed in this pull request?
    
    Allow converting `bytes::Bytes` into a Binary Scalar.
    
    <!--
    Please clarify what changes you are proposing and why the changes are
    needed.
    The purpose of this section is to outline the changes, why they are
    needed, and how this PR fixes the issue.
    If the reason for the change is already explained clearly in an issue,
    then it does not need to be restated here.
    1. If you propose a new API or feature, clarify the use case for a new
    API or feature.
      2. If you fix a bug, you can clarify why it is a bug.
    -->
    
    <!--
    Uncomment this section if there are any changes affecting public APIs:
    ### This PR affects the following public APIs
    
    If there are breaking changes, please ensure the `breaking-changes`
    label gets added by CI, and describe why the changes are needed.
    
    Note that _new_ public APIs are not considered breaking.
    -->
    
    
    ## How was this change tested?
    <!--
    Please make sure to add test cases that check the changes thoroughly
    including negative and positive cases if possible.
    If it was tested in a way different from regular unit tests, please
    clarify how you tested, ideally via a reproducible test documented in
    the PR description.
    -->

--- kernel/src/expressions/scalars.rs
+++ kernel/src/expressions/scalars.rs
@@ -546,6 +546,12 @@ impl From<&[u8]> for Scalar {
     }
 }
 
+impl From<bytes::Bytes> for Scalar {
+    fn from(b: bytes::Bytes) -> Self {
+        Self::Binary(b.into())
+    }
+}
+
 impl<T> TryFrom<Vec<T>> for Scalar
 where
     T: Into<Scalar> + ToDataType,
@@ -1238,5 +1244,36 @@ mod tests {
         }
 
         Ok(())
+    }
+
+    #[test]
+    fn test_bytes_conversion() {
+        // Test with non-empty bytes
+        let bytes = bytes::Bytes::from(vec![1, 2, 3, 4, 5]);
+        let scalar: Scalar = bytes.into();
+
+        // Verify the scalar is of Binary type
+        assert!(matches!(scalar, Scalar::Binary(_)));
+
+        // Verify the data type
+        assert_eq!(scalar.data_type(), DataType::BINARY);
+
+        // Extract the binary data and verify contents
+        if let Scalar::Binary(data) = scalar {
+            assert_eq!(data, vec![1, 2, 3, 4, 5]);
+        } else {
+            panic!("Expected Binary scalar");
+        }
+
+        // Test with empty bytes
+        let empty_bytes = bytes::Bytes::new();
+        let empty_scalar: Scalar = empty_bytes.into();
+
+        assert!(matches!(empty_scalar, Scalar::Binary(_)));
+        if let Scalar::Binary(data) = empty_scalar {
+            assert!(data.is_empty());
+        } else {
+            panic!("Expected Binary scalar");
+        }
     }
 }
